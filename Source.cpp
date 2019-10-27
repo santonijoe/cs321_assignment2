5c6a9bb447e279cb7db6cbb5821ae76ac919a84f
evbmey <55164801+evbmey@users.noreply.github.com>
2019-10-27 15:03:24 -0500

    added source file

---
+++ Source.cpp
@@ -0,0 +1,49 @@
+int main() {
+
+…
+
+setJobQueues();           /* Set up the priority job queues with chosen file and data structure */
+
+if (pid=fork() > 0) {/* Parent, jobGenerator process */
+
+    jobGenerator(…); /* generate random jobs and put them into the priority queues
+
+                                         The priority queues must be protected in a critical region */
+
+} else {/* Child, job scheduler process */
+
+    jobScheduler(…); /* schedule and execute the jobs. */
+
+    exit(0);
+
+}
+
+return (1);
+
+}
+
+void jobScheduler(…) {
+
+  …
+
+  while (i < N) {           /* schedule and run maximum N jobs */
+
+     n=selectJob(…)      /* pick a job from the job priority queues */
+
+     if (n>0) { /* valid job id */
+
+                        if (pid = fork() == 0) { /* child worker process */
+
+                            executeJob (…);      /* execute the job */
+
+                            exit(0);
+
+                        }
+
+}
+
+i++;
+
+  }
+
+}
